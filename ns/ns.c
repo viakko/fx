a0d548433f4cf3a5a61451e0691de4d055d602a6
viakko <redgogh@vip.qq.com>
2025-11-27 15:48:38 +0800

    ns: simplify flow by using direct exit for subcommands

--- ns/ns.c
+++ ns/ns.c
@@ -13,79 +13,79 @@
 #include <fx/typedefs.h>
 
 /* --flushdns */
-static void handle_flushdns()
+static int flush_dns(void)
 {
-        int ret = -1;
+        int r;
 
 #ifdef _WIN32
-        system("ipconfig /flushdns");
+        r = system("ipconfig /flushdns");
 #elif __linux__
-        system("sudo systemd-resolve --flush-caches");
+        r = system("sudo systemd-resolve --flush-caches");
 #elif __APPLE__
-        // try macOS 13 (Ventura)
-        ret = system("sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder 2>/dev/null");
-
-        if (ret != 0) {
-                // try macOS 12 (Monterey)
-                ret = system("sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder 2>/dev/null");
-        }
-
-        if (ret != 0) {
-                // try macOS 11 (Big Sur)
-                ret = system("sudo killall -HUP mDNSResponder 2>/dev/null");
-        }
+        r = system("sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder 2>/dev/null");
+        if (r != 0)
+                r = system("sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder 2>/dev/null");
+        if (r != 0)
+                r = system("sudo killall -HUP mDNSResponder 2>/dev/null");
 #endif
-
-        if (ret == 0) {
+        if (r == 0)
                 printf("DNS cache flushed successfully.\n");
-        } else {
+        else
                 printf("Failed to flush DNS cache.\n");
-        }
+
+        return r;
 }
 
-static void show_dns_list()
+static int show_dns()
 {
         struct __res_state res;
 
         if (res_ninit(&res) != 0) {
                 perror("res_ninit failed");
-                exit(1);
+                return 1;
         }
 
         for (int i = 0; i < res.nscount; i++)
                 printf("%s\n", inet_ntoa(res.nsaddr_list[i].sin_addr));
 
         res_nclose(&res);
-        exit(0);
+
+        return 0;
 }
 
-static void show_resolv_conf()
+static int show_resolv_conf()
 {
-        FILE *fp = fopen("/etc/resolv.conf", "r");
-        if (!fp)
-                die("Cannot open /etc/resolv.conf");
-
         char buf[4096];
+        FILE *fp;
+
+        fp = fopen("/etc/resolv.conf", "r");
+        if (!fp) {
+                perror("cannot open /etc/resolv.conf");
+                return 1;
+        }
+
         while (fgets(buf, sizeof(buf), fp))
-                printf("%s\n", buf);
+                fputs(buf, stdout);
 
         fclose(fp);
+
+        return 0;
 }
 
 int main(int argc, char **argv)
 {
         struct argparser *ap;
-        struct option *dns;
-        struct option *resolv;
-        struct option *flushdns;
+        struct option *opt_dns;
+        struct option *opt_resolv;
+        struct option *opt_flush_dns;
 
         ap = argparser_create();
         if (!ap)
                 return -1;
 
-        argparser_add1(ap, &dns, "dns", NULL, "show resolv DNS address and exit", OP_NULL);
-        argparser_add0(ap, &resolv, "resolv", NULL, "show resolv.conf and exit", OP_NULL);
-        argparser_add0(ap, &flushdns, NULL, "flushdns", "flush system DNS cache and exit", OP_NULL);
+        argparser_add1(ap, &opt_dns, "dns", NULL, "show resolv DNS address and exit", OP_NULL);
+        argparser_add0(ap, &opt_resolv, "resolv", NULL, "show resolv.conf and exit", OP_NULL);
+        argparser_add0(ap, &opt_flush_dns, NULL, "flushdns", "flush system DNS cache and exit", OP_NULL);
 
         if (argparser_run(ap, argc, argv) != 0) {
                 fprintf(stderr, "%s\n", argparser_error(ap));
@@ -93,9 +93,14 @@ int main(int argc, char **argv)
                 return -1;
         }
 
-        if (dns) show_dns_list();
-        if (resolv) show_resolv_conf();
-        if (flushdns) handle_flushdns();
+        if (opt_dns)
+                return show_dns();
+
+        if (opt_resolv)
+                return show_resolv_conf();
+
+        if (opt_flush_dns)
+                return flush_dns();
 
         return 0;
 }
