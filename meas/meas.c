c8d87a94f3f7d1c1a943b509a627615c4928b349
viakko <redgogh@vip.qq.com>
2025-11-28 16:25:03 +0800

    meas: simplify call meas options

--- meas/meas.c
+++ meas/meas.c
@@ -6,49 +6,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <argparser.h>
+#include <ctype.h>
 #include <string.h>
+#include <fx/typedefs.h>
 
-#define MEAS_VERSION "1.0"
+#define MEAS_VERSION "1.0.0"
 
-static size_t strlen_utf8(const char *str)
+static size_t __strlen_utf8(const char *str) // NOLINT(*-reserved-identifier)
 {
         size_t len = 0;
 
         while (*str) {
                 unsigned char c = (unsigned char) *str;
+                str++;
+
+                if (isspace(c))
+                        continue;
+
                 if ((c & 0xC0) != 0x80)
                         len++;
-                str++;
         }
 
         return len;
 }
 
-static void run_help(struct argparser *ap)
+static size_t length(const char *str, int is_unicode)
 {
-        printf("%s", argparser_help(ap));
-        exit(0);
-}
-
-static void run_version(void)
-{
-        printf("meas version: %s\n", MEAS_VERSION);
-        exit(0);
-}
-
-static int run_strlen(int is_unicode, const char *str)
-{
-        printf("%zu\n", is_unicode ? strlen_utf8(str) : strlen(str));
-        return 0;
+        return is_unicode ? __strlen_utf8(str) : strlen(str);
 }
 
 int main(int argc, char **argv)
 {
         struct argparser *ap;
-        struct option *opt_help;
-        struct option *opt_version;
-        struct option *opt_str;
-        struct option *opt_uni;
+        struct option *help;
+        struct option *version;
+        struct option *str;
+        struct option *unicode;
 
         ap = argparser_create("meas");
         if (!ap) {
@@ -56,10 +49,10 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-        argparser_add0(ap, &opt_help, "h", "help", "show this help message and exit", OPT_NONE);
-        argparser_add0(ap, &opt_version, "version", NULL, "show current version", OPT_NONE);
-        argparser_add1(ap, &opt_str, "s", "str", "input string value", OPT_NONE | OPT_REQVAL);
-        argparser_add0(ap, &opt_uni, "u", "unicode", "use unicode parse string length", OPT_NONE);
+        argparser_add0(ap, &help, "h", "help", "show this help message and exit", OPT_NONE);
+        argparser_add0(ap, &version, "version", NULL, "show current version", OPT_NONE);
+        argparser_add0(ap, &str, "s", "str", "as string type", OPT_NONE);
+        argparser_add0(ap, &unicode, "u", "unicode", "use unicode parse string length", OPT_NONE);
 
         if (argparser_run(ap, argc, argv) != 0) {
                 fprintf(stderr, "%s\n", argparser_error(ap));
@@ -67,21 +60,20 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-        if (opt_help)
-                run_help(ap);
-
-        if (opt_version)
-                run_version();
-
-        if (opt_str || argparser_count(ap) > 0) {
-                if (!opt_str) {
-                        /* Default execute get unicode string length */
-                        run_strlen(1, argparser_val(ap, 0));
-                } else {
-                        run_strlen(opt_uni != NULL, opt_str->sval);
-                }
+        if (help) {
+                printf("%s", argparser_help(ap));
+                goto cleanup;
         }
 
+        if (version) {
+                printf("Meas %s\n", MEAS_VERSION);
+                goto cleanup;
+        }
+
+        if (argparser_count(ap) > 0)
+                printf("%zu\n", length(argparser_val(ap, 0), !IS_NULL(unicode)));
+
+cleanup:
         argparser_free(ap);
 
         return 0;
