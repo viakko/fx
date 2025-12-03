9b1951ac9a57160342cccd1e6480b798c519c8ff
viakko <redgogh@vip.qq.com>
2025-12-03 13:42:54 +0800

    ns: add -flushdns option to flush DNS caches in system

--- ns/ns.c
+++ ns/ns.c
@@ -28,11 +28,32 @@ static int on_dns(struct argparser *ap, struct option *opt)
         exit(0);
 }
 
+static int on_flush_dns(struct argparser *ap, struct option *opt)
+{
+        int ret = 0;
+
+        ret |= system("dscacheutil -flushcache >/dev/null 2>&1");
+        ret |= system("killall -HUP mDNSResponder >/dev/null 2>&1");
+
+        // optional macOS 12+ process, failure is non-critical.
+        ret |= system("killall mDNSResponderHelper >/dev/null 2>&1");
+        ret |= system("killall -HUP mDNSResponderHelper >/dev/null 2>&1");
+
+        if (ret == 0) {
+                puts("DNS cache flushed.");
+                return 0;
+        }
+
+        fprintf(stderr, "Failed to flush DNS cache (need sudo?)");
+        return 1;
+}
+
 int main(int argc, char **argv)
 {
         struct argparser *ap;
         struct option *help, *version;
         struct option *dns;
+        struct option *flushdns;
 
         ap = argparser_create("ns", NS_VERSION);
         if (!ap)
@@ -41,12 +62,15 @@ int main(int argc, char **argv)
         argparser_add0(ap, &help, "h", "help", "show this help message and exit", __acb_help, opt_none);
         argparser_add0(ap, &version, "version", NULL, "show version", __acb_version, opt_none);
         argparser_add1(ap, &dns, "dns", NULL, "show resolv DNS address and exit", on_dns, opt_none);
+        argparser_add1(ap, &flushdns, "flushdns", NULL, "flush DNS caches", on_flush_dns, opt_none);
 
         if (argparser_run(ap, argc, argv) != 0) {
                 fprintf(stderr, "%s\n", argparser_error(ap));
                 argparser_free(ap);
                 return -1;
         }
+
+        argparser_free(ap);
 
         return 0;
 }
