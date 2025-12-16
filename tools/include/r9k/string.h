14744e2cabd47b0174953fee0a96b516ded11150
viakko <redgogh@vip.qq.com>
2025-12-16 11:06:42 +0800

    string: remove r9k prefix in header file ifdef

--- tools/include/r9k/string.h
+++ tools/include/r9k/string.h
@@ -2,11 +2,11 @@
 -* SPDX-License-Identifier: MIT
  * Copyright (c) 2025 viakko
  */
-#ifndef R9K_STRING_H_
-#define R9K_STRING_H_
+#ifndef STRING_H_
+#define STRING_H_
 
 #include <string.h>
 
 int is_str_blank(const char *str);
 
-#endif /* R9K_STRING_H_ */
+#endif /* STRING_H_ */
