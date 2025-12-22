b4ede43fc159b749ea3170ecfde74f9aefe78f70
viakko <redgogh@vip.qq.com>
2025-12-22 15:57:33 +0800

    attrs: add __attr_always_inline macro

--- include/r9k/compiler_attrs.h
+++ include/r9k/compiler_attrs.h
@@ -9,15 +9,17 @@
 #define __attr_ignore2(a, b) __attr_ignore(a), __attr_ignore(b)
 
 #if defined(__GNUC__) || defined(__clang__)
-#  define __attr_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
-#  define __attr_unused       __attribute__((unused))
-#  define __attr_noreturn     __attribute__((noreturn))
-#  define __attr_printf(a, b) __attribute__((format(printf, a, b)))
+#  define __attr_nonnull(...)  __attribute__((nonnull(__VA_ARGS__)))
+#  define __attr_unused        __attribute__((unused))
+#  define __attr_noreturn      __attribute__((noreturn))
+#  define __attr_printf(a, b)  __attribute__((format(printf, a, b)))
+#  define __attr_always_inline __attribute__((always_inline))
 #else
 #  define __attr_nonnull(...)
 #  define __attr_unused
 #  define __attr_noreturn
 #  define __attr_printf(a, b)
+#  define __attr_always_inline
 #endif
 
 #endif /* COMPILER_ATTRUBUTES_H_ */
