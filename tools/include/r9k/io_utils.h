2ae3c9b9bdeb26cee4095820ddbf0dcd3ab54398
viakko <redgogh@vip.qq.com>
2026-01-08 09:54:12 +0800

    io_utils: algin space

--- tools/include/r9k/io_utils.h
+++ tools/include/r9k/io_utils.h
@@ -8,14 +8,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	  *slurp(int fd, size_t *len);
+void   *slurp(int fd, size_t *len);
 ssize_t	dump(int fd, const void *buf, size_t count);
-void	  *fslurp(FILE *stream, size_t *len);
+void   *fslurp(FILE *stream, size_t *len);
 ssize_t	fdump(FILE *stream, const void *buf, size_t count);
 
-char	  *stream_read_c(FILE *stream);
+char   *stream_read_c(FILE *stream);
 void   *path_read_v(const char *path, size_t *len);
-char	  *path_read(const char *path);
+char   *path_read(const char *path);
 ssize_t path_write(const char *path, const void *buf, size_t count);
 
 /* io_buffer_read/io_buffer_write: Read/write up to count bytes from/to fd using buf,
