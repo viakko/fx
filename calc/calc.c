aa76d8a8de6432bbccb9827708fbca7e0789c9ec
viakko <redgogh@vip.qq.com>
2026-01-08 15:37:07 +0800

    calc: prefix 0 to bc output starting with '.'

--- calc/calc.c
+++ calc/calc.c
@@ -11,11 +11,11 @@ int main(int argc, char* argv[])
         if (argc != 2)
                 return 1;
 
-        fp = popen("bc -l", "w");
+        fp = popen("bc -l | sed 's/^\\./0./'", "w");
         if (!fp)
                 return 1;
 
-        fprintf(fp, "%s\n", argv[1]);
+        fprintf(fp, "scale=2; %s\n", argv[1]);
         pclose(fp);
 
         return 0;
